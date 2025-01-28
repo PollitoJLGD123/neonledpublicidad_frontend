2ee9d5c65fe18e813c640e443d3f66ac8bcad9e3
brayan anthony alaya atalaya <brayan.alaya@hotmail.com>
2025-01-28 11:10:41 -0500

    colores y enlaces

--- app/(client)/components/header/Header.jsx
+++ app/(client)/components/header/Header.jsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
         <>
-            <h1>Header</h1>
+            <header className="h-[60px] bg-[--azul_oscuro] flex items-center justify-center gap-6 ">
+                <Link href="/" className="text-white">Home</Link>
+                <Link href="/contacto" className="text-white">Contacto</Link>
+                <Link href="/nosotros" className="text-white">Nosotros</Link>
+                <Link href="/productos" className="text-white">Productos</Link>
+                <Link href="/reclamaciones" className="text-white">Reclamaciones</Link>
+            </header>
         </>
     );
 }
