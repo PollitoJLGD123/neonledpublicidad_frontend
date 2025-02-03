74d12b185c4d50254498df1a6491336fc0482723
brayan anthony alaya atalaya <brayan.alaya@hotmail.com>
2025-02-03 13:11:15 -0500

    cambios en los productos

--- app/(client)/productos/(client)/displays/page.jsx
+++ app/(client)/productos/(client)/displays/page.jsx
@@ -1,9 +1,15 @@
-import Section2 from "../components/Section2";
+import Banner from "../components/banner";
+import Datos from "../components/datos";
+import Ejemplos from "../components/Ejemplos";
+import Section2 from "../components/section2/Section2";
 
 export default function Home() {
   return (
     <>
+      <Banner />
       <Section2 />
+      <Datos />
+      <Ejemplos />
     </>
   );
 }
