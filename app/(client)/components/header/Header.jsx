d122b7e43aa054fe6072c51a6642e19fa333e411
artjs120820 <arturosilvera1101@gmail.com>
2025-02-06 11:45:06 -0500

    Nav responsive v2

--- app/(client)/components/header/Header.jsx
+++ app/(client)/components/header/Header.jsx
@@ -1,68 +1,127 @@
 "use client";
-
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import DropdownLink from "./components/DropdownLink";
 import LinkNav from "./components/LinkNav";
 import "./header.css";
-
 export default function Header() {
     const [menuActive, setMenuActive] = useState(false);
+    const [containerFullHeight, setContainerFullHeight] = useState(false);
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
+    const [menuInitialized, setMenuInitialized] = useState(false);
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth <= 580) {
+                setIsSmallScreen(true);
+                if (!menuInitialized) {
+                    setMenuActive(true);
+                    setMenuInitialized(true);
+                    setContainerFullHeight(true);
+                }
+            } else {
+                setIsSmallScreen(false);
+                setMenuActive(false);
+                setContainerFullHeight(false);
+                setMenuInitialized(false);
+            }
+        };
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, [menuInitialized]);
+
+    useEffect(() => {
+        if (menuActive) {
+            document.body.classList.add("overflow-hidden");
+        } else {
+            document.body.classList.remove("overflow-hidden");
+        }
+        return () => {
+            document.body.classList.remove("overflow-hidden");
+        };
+    }, [menuActive]);
 
     const toggleMenu = () => {
-        setMenuActive(!menuActive); // Cambiar entre true y false
+        setMenuActive(!menuActive);
+        if (!menuActive) {
+            setContainerFullHeight(true);
+        } else {
+            setContainerFullHeight(false);
+        }
     };
 
     return (
         <>
-            <header
-                className={`header-container h-[100px] bg-[--azul_oscuro] flex items-center justify-center gap-6 ${
-                    menuActive ? "menu-active" : ""
-                }`}
-            >
-                <LinkNav text={"Inicio"} link={"/"} />
-                <LinkNav text={"Productos"} link={"/productos"} />
-                <div className="h-[50px] w-[50px] text-white mx-7 text-center logo">Neon Led</div>
-                <LinkNav text={"Nosotros"} link={"/nosotros"} />
-                <LinkNav text={"Contacto"} link={"/contacto"} />
-                {/* Icono de menú para pantallas pequeñas */}
-                <div className="menu-icon" onClick={toggleMenu}>
-                    {/* Si el menú está activo, muestra "Cerrar X", si no muestra el ícono ≡ */}
-                    <span className="menu-icon-text">
-                        {menuActive ? "Cerrar X" : "\u2630"}
-                    </span>
-                </div>
-            </header>
+            <div className={`containerF ${menuActive ? "full-height" : ""}`}>
+                <header
+                    className={`header-container h-[100px] bg-[--azul_oscuro] flex items-center justify-center gap-6 ${menuActive ? "menu-active" : ""}`}
+                >
+                    <LinkNav text={"Inicio"} link={"/"} />
+                    <LinkNav text={"Productos"} link={"/productos"} />
+                    <div className="h-[50px] w-[50px] text-white mx-7 text-center logo">Neon Led</div>
+                    <LinkNav text={"Nosotros"} link={"/nosotros"} />
+                    <LinkNav text={"Contacto"} link={"/contacto"} />
+                    {isSmallScreen && (
+                        <div className="menu-icon" onClick={toggleMenu}>
+                            <span className="menu-icon-text">
+                                {menuActive ? "Cerrar X" : "\u2630"}
+                            </span>
+                        </div>
+                    )}
+                </header>
+                {menuActive && (
+                    <>
+                        <div className="dropdown-menu bg-gradient-to-r from-[--azul_brillante] to-[--azul_intenso]">
+                            <DropdownLink
+                                text={"Inicio"}
+                                link={"/"}
+                                isInicio={true}
+                                final={false}
+                                closeMenu={() => {
+                                    setMenuActive(false);
+                                    setContainerFullHeight(false);
+                                }}
+                            />
+                            <DropdownLink
+                                text={"Productos"}
+                                link={"/productos"}
+                                isInicio={false}
+                                final={false}
+                                closeMenu={() => {
+                                    setMenuActive(false);
+                                    setContainerFullHeight(false);
+                                }}
+                            />
+                            <DropdownLink
+                                text={"Nosotros"}
+                                link={"/nosotros"}
+                                isInicio={false}
+                                final={false}
+                                closeMenu={() => {
+                                    setMenuActive(false);
+                                    setContainerFullHeight(false);
+                                }}
+                            />
+                            <DropdownLink
+                                text={"Contacto"}
+                                link={"/contacto"}
+                                isInicio={false}
+                                final={true}
+                                closeMenu={() => {
+                                    setMenuActive(false);
+                                    setContainerFullHeight(false);
+                                }}
+                            />
+                            <div className="red-bg ">
+                                <img className="absolute w-[130px] right-[calc(50%-65px)] bottom-[0] translate-y-[40%] z-30" src="/header_footer/tiktock.svg" alt="" />
+                            </div>
+                        </div>
 
-            {/* Menú desplegable cuando está activo */}
-            {menuActive && (
-                <div className="dropdown-menu">
-                    <DropdownLink
-                        text={"Inicio"}
-                        link={"/"}
-                        isInicio={true}
-                        closeMenu={() => setMenuActive(false)} // Cierra el menú al hacer clic
-                    />
-                    <DropdownLink
-                        text={"Productos"}
-                        link={"/productos"}
-                        isInicio={false}
-                        closeMenu={() => setMenuActive(false)} // Cierra el menú al hacer clic
-                    />
-                    <DropdownLink
-                        text={"Nosotros"}
-                        link={"/nosotros"}
-                        isInicio={false}
-                        closeMenu={() => setMenuActive(false)} // Cierra el menú al hacer clic
-                    />
-                    <DropdownLink
-                        text={"Contacto"}
-                        link={"/contacto"}
-                        isInicio={false}
-                        closeMenu={() => setMenuActive(false)} // Cierra el menú al hacer clic
-                    />
-                </div>
-            )}
+                    </>
+                )}
+            </div>
         </>
     );
 }
--- app/(client)/components/header/components/DropdownLink.jsx
+++ app/(client)/components/header/components/DropdownLink.jsx
@@ -1,20 +1,17 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export default function DropdownLink({ text, link, isInicio, closeMenu }) {
+export default function DropdownLink({ text, link, isInicio, closeMenu, final}) {
     const pathname = usePathname();
 
     return (
         <Link
             href={link}
-            onClick={closeMenu} // Cerrar el menú al hacer clic
-            className={`flex items-center justify-between gap-2 py-2 px-4 pt-4 pb-4 text-white font-bold
-                ${pathname === link ? "" : "bg-transparent"} 
-                border-t-2 border-t-[--celeste] hover:bg-[--azul_claro]`}
+            onClick={closeMenu} 
+            className={`flex items-center justify-between gap-2 py-4 px-4 text-white font-bold
+                ${pathname === link ? "bg-[--azul_claro]" : "bg-[--azul_oscuro]"} ${final ? "border-b-2 border-b-[--azul_brillante]" : ""} border-t-2 border-t-[--celeste] hover:bg-[--azul_claro] `}
         >
             <span>{text}</span>
-
-            {/* Ícono alineado a la derecha */}
             {!isInicio && (
                 <span className="w-0 h-0 border-l-[6px] border-r-[6px] border-t-[12px] border-l-transparent border-r-transparent border-t-white -rotate-90"></span>
             )}
