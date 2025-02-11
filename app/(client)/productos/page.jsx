2a01684f28da0d985aa1d5d701c8a593e2573037
artjs120820 <arturosilvera1101@gmail.com>
2025-02-11 12:59:22 -0500

    seo productos NHL

--- app/(client)/productos/page.jsx
+++ app/(client)/productos/page.jsx
@@ -3,6 +3,38 @@ import NeonBackground from "./components/Luz";
 import NuestrosProductos from "./components/NuestrosProductos";
 import Productos from "./components/ProductosPrincipal";
 
+export const metadata = {
+  title: "Productos Personalizados - Descubre lo Mejor",
+  description:
+    "Explora nuestra amplia gama de productos personalizados con características únicas, ventajas innovadoras y tecnología avanzada. Ideal para negocios y hogares.",
+  keywords: [
+    "neón LED",
+    "letras de acrílico",
+    "letreros luminosos",
+    "productos personalizados",
+    "iluminación LED",
+    "letreros personalizados",
+    "letreros de neón",
+    "letras luminosas",
+    "decoración con neón",
+    "letreros para eventos",
+    "letreros publicitarios",
+    "letreros LED personalizados",
+    "decoración para negocios",
+    "decoración para oficinas",
+    "decoración para el hogar",
+    "diseño en acrílico",
+    "tecnología LED avanzada",
+    "regalos personalizados",
+    "ideas de iluminación personalizada",
+    "iluminación artística",
+    "letreros modernos",
+    "decoración minimalista",
+    "diseño único en acrílico",
+    "decoración estética",
+    "decoración para bodas",
+  ],
+};
 
 export default function Home() {
   return (
@@ -12,7 +44,7 @@ export default function Home() {
       <div className="relative z-10">
         <Banner />
         <NuestrosProductos />
-        <Productos/>
+        <Productos />
       </div>
     </div>
   );
