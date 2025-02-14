05f83463af804d34ffa0debfc5858c920f3ff8f9
artjs120820 <arturosilvera1101@gmail.com>
2025-02-14 12:27:21 -0500

    card centrados productos fix

--- app/(client)/productos/components/ProductoIndividual.jsx
+++ app/(client)/productos/components/ProductoIndividual.jsx
@@ -3,14 +3,13 @@ import './productoStyles.css';
 
 function Producto({ imgSrc, altText, description, route }) {
   return (
-    <a href={route} className="producto-link flex justify-center">
-      <div className="producto flex flex-col items-center text-center">
+    <a href={route} className="producto-link">
+      <div className="producto">
         <img src={imgSrc} alt={altText} className="producto-img" />
         <h3 className="producto-description">{description}</h3>
       </div>
     </a>
   );
 }
-
 
 export default Producto;
