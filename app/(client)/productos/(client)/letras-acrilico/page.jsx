8c156c8be42645034a1b8ea7ed04da3a850d6c63
francesca silva <frannysilva@hotmail.com>
2025-02-26 13:09:11 -0500

    imagenes a productos

--- app/(client)/productos/(client)/letras-acrilico/page.jsx
+++ app/(client)/productos/(client)/letras-acrilico/page.jsx
@@ -6,6 +6,25 @@ import Section2 from '../components/section2/Section2';
 
 export default function Home() {
   const idProducto = 1;
+  const cards = [
+    { 
+      title: "LETRAS DE ACRÍLICO", 
+      description: "Te mostramos la implementación de las letras de acrílico en diversos espacios", 
+      bgColor:"bg-gray-900 text-white px-4 py-6 rounded-lg flex flex-col justify-center items-center",
+      glow: "text-white-400 text-3xl font-bold tracking-wide mb-4",
+     textStyle: "text-white text-center max-w-[50%] leading-relaxed text-lg whitespace-pre-line"
+    },
+    { 
+      title: "Juguetería", 
+      description: "Espacio interior", 
+      image: "/Productos/ACRÍLICO1PRODUCTO.jpg" 
+    },
+    { 
+      title: "Feria", 
+      description: "Espacio interior", 
+      image: "/Productos/ACRÍLICO2PRODUCTO.jpg" 
+    }
+  ];
   return (
     <>
       <Banner
@@ -14,7 +33,7 @@ export default function Home() {
       />
       <Section2 idProducto={idProducto} />
       <Datos idProducto={idProducto}/>
-      <CardSlider/>
+      <CardSlider cards={cards}/>
     </>
   );
 }
