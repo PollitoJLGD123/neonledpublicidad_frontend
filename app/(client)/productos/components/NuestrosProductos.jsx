0512b910beca50b630a431c9c053bf5183b420f6
Jose Luis <joseluisjlgd123@gmail.com>
2025-03-11 09:32:49 -0500

    Se agrego cambios de nosotros y nuestrosproductos

--- app/(client)/productos/components/NuestrosProductos.jsx
+++ app/(client)/productos/components/NuestrosProductos.jsx
@@ -13,34 +13,45 @@ export default function NuestrosProductos() {
   }
 
   return (
-    <div ref={ref} className="flex items-center justify-center space-x-12 mt-[8%] overflow-hidden">
+    <div ref={ref} className="flex flex-col items-center text-center md:flex-row md:text-left justify-center gap-4 md:gap-12 mt-[5%] overflow-hidden">
+
       {/* Título */}
       <motion.div
-        className="text-4xl font-bold text-white"
+        className="text-2xl md:text-2xl font-bold text-white"
         initial={{ x: -200, opacity: 0 }}
         animate={hasAnimated ? { x: 0, opacity: 1 } : {}}
         transition={{ duration: 2, ease: "easeOut" }}
       >
-        Nuestros Productos
+        NUESTROS PRODUCTOS
       </motion.div>
 
-      {/* Línea vertical */}
+
+      {/* Línea vertical (solo en pantallas medianas y grandes) */}
       <motion.div
-        className="border-r-4 h-12 border-[#44b0f8] rounded-full"
+        className="hidden md:block border-r-4 h-12 border-[#44b0f8] rounded-full"
         initial={{ scaleY: 0, opacity: 0 }}
         animate={hasAnimated ? { scaleY: 1, opacity: 1 } : {}}
         transition={{ duration: 2, ease: "easeOut" }}
       />
 
+      {/* Línea horizontal (solo en móviles) */}
+      <motion.div
+        className="block md:hidden h-1 w-16 bg-[#44b0f8] rounded-full"
+        initial={{ scaleX: 0, opacity: 0 }}
+        animate={hasAnimated ? { scaleX: 1, opacity: 1 } : {}}
+        transition={{ duration: 2, ease: "easeOut" }}
+      />
+
       {/* Texto */}
       <motion.div
-        className="text-xl text-white max-w-3xl text-left"
+        className="text-xl text-white max-w-3xl text-center md:text-left leading-relaxed"
         initial={{ x: 200, opacity: 0 }}
         animate={hasAnimated ? { x: 0, opacity: 1 } : {}}
         transition={{ duration: 2, ease: "easeOut" }}
       >
         Ofrecemos una gran variedad de productos en NEON LED para tu negocio tanto exterior como interior.
       </motion.div>
+
     </div>
   );
 }
