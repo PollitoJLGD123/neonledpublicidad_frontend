606be94d62b7785a1d48656d23b26d4f76fb8da1
Jose Luis <joseluisjlgd123@gmail.com>
2025-03-11 20:21:26 -0500

    Se cambio el diseño del blog plantilla

--- app/(client)/blog/components/SidebarMenu/SidebarMenu.jsx
+++ app/(client)/blog/components/SidebarMenu/SidebarMenu.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SidebarMenu = () => {
   return (
-    <div className="hidden lg:block fixed right-1 top-[25%] bg-white p-4 rounded-l-lg shadow-lg w-56 z-50 border-black">
+    <div className="hidden lg:block fixed top-28 right-1 bg-white p-4 rounded-l-lg shadow-lg w-56 z-50 border border-black max-h-[80vh] overflow-y-auto">
       <div className="mb-4">
         <h3 className="text-xl font-bold text-center mb-4">¡Descubre!</h3>
       </div>
@@ -12,16 +12,7 @@ const SidebarMenu = () => {
           Letreros
         </a>
         
-        {/* Links con degradado */}
-        {[
-          'Carteles',
-          'Impresiones',
-          'Otros',
-          'Consultas',
-          'Tendencias',
-          'Instalaciones',
-          'Garantía'
-        ].map((item, index) => (
+        {[ 'Carteles', 'Impresiones', 'Otros', 'Consultas', 'Tendencias', 'Instalaciones', 'Garantía' ].map((item, index) => (
           <a 
             key={index}
             href="#" 
--- app/(client)/blog/content/blog4/page.jsx
+++ app/(client)/blog/content/blog4/page.jsx
@@ -29,113 +29,71 @@ export default function Blog4() {
         </div>
       </div>
       
-      {/* Integramos el sidebar menu independiente del contenido principal */}
+      { /*Integramos el sidebar menu independiente del contenido principal*/ }
       <SidebarMenu />
-      
-      {/* Contenido del blog con margen superior sobre fondo blanco y barra lateral celeste */}
+  
       <div className="container mx-auto px-4 py-12 relative bg-gradient-to-r text-black min-h-screen w-full">
-        {/* Barra lateral celeste con ancho adaptativo */}
-        <div className="hidden lg:block w-20 xl:w-24 2xl:w-32 bg-[--azul_brillante] fixed left-0 top-0 h-full -z-10"></div>
+
+        <div className="hidden lg:block w-20 xl:w-24 2xl:w-32 bg-gradient-to-b from-red-500 via-white to-blue-500 fixed left-0 top-0 h-full -z-10"></div>
         
-        {/* Contenido del artículo - Ajustado con margen derecho adicional */}
-        <div className="relative lg:ml-48 lg:mr-48 justify-center">
-          <div className='flex flex-col xl:flex-row lg:gap-4 '>
-            {/* Título del artículo con fecha */}
-            <div className='w-full lg:w-3/2'>
-              <div className="flex items-center mb-8 mt-5">
-                <h2 className="text-4xl font-bold">Tu Bar, en la Mira</h2>
-                <p className="ml-4 text-sm text-gray-500">6 de marzo de 2023</p>
-              </div>             
-              <p className="text-lg mb-6">
+        <div className="relative lg:mx-48 p-6 bg-black/90 text-white rounded-lg shadow-xl">
+          {/* Contenido principal */}
+          <div className="flex flex-col xl:flex-row lg:gap-6">
+            <div className='w-full xl:w-1/2'>
+              <div className="flex items-center mb-6 mt-5">
+                <h2 className="text-5xl font-extrabold text-red-500">Tu Bar, en la Mira</h2>
+                <p className="ml-4 text-lg text-gray-400">6 de marzo de 2023</p>
+              </div>
+              <p className="text-lg leading-relaxed">
                 Las luces neón LED se han convertido en un elemento diferenciador en el mundo de la hospitalidad. No solo son visualmente atractivos, sino que también refuerzan la identidad de tu negocio. En este artículo, exploraremos cómo las letras luminosas pueden marcar la diferencia en la experiencia de tus clientes.
               </p>
             </div>
-            <figure className='flex align-middle justify-center w-full xl:w-1/2'>
-              <img src="/blog/blog4/LETRA_NEON.png" alt="Letrero de neón en un bar" className="w-[400px] lg:w-[320px]  max-w-2xl rounded-lg shadow-lg " />
+            <figure className='flex justify-center w-full xl:w-1/2'>
+              <img src="/blog/blog4/LETRA_NEON.png" alt="Letrero de neón en un bar" className="w-80 xl:w-96 rounded-lg shadow-lg border-2 border-red-500" />
             </figure>
-            
           </div>
-          
-          
-          
-          <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 mb-10 mt-20'>
-              {/* Sección 1 */}
-            <div className="mb-5">
-              <h3 className="text-2xl font-bold mb-3">1. El Factor Sorpresa y Distinción</h3>
-              <p className="mb-4">
-                Las letras de neón LED permiten personalizar la imagen de tu local, haciendo que el nombre de tu bar sea visible desde lejos. Un diseño llamativo puede convertirse en un sello distintivo y en un punto de referencia para los clientes.
-              </p>
-            </div>
-            
-            {/* Sección 2 */}
-            <div className="mb-5">
-              <h3 className="text-2xl font-bold mb-3">2. Ambiente y Experiencia Visual</h3>
-              <p className="mb-4">
-                La iluminación juega un papel crucial en la atmósfera de un bar. Los colores vibrantes y cálidos del neón LED pueden transformar un espacio ordinario en un entorno acogedor e instagrameable, incentivando a los clientes a compartir su experiencia en redes sociales.
-              </p>
-              
-            </div>
-            {/* Sección 3 */}
-            <div className="mb-5">
-              <h3 className="text-2xl font-bold mb-3">3. Eficiencia Energética y Durabilidad</h3>
-              <p className="mb-4">
-                A diferencia del neón tradicional, las luces LED son más eficientes, consumen menos energía y tienen una vida útil más prolongada. Esto no solo reduce costos operativos, sino que también contribuye a un enfoque más sostenible para tu negocio.
-              </p>
-            </div>
-            
-            {/* Sección 4 */}
-            <div className="mb-5">
-              <h3 className="text-2xl font-bold mb-3">4. Marketing y Atracción de Clientes</h3>
-              <p className="mb-4">
-                Un letrero de neón LED bien diseñado es una herramienta de marketing poderosa, capaz de captar la atención, diferenciarte de la competencia y aumentando la visibilidad de tu local.
-              </p>
-            </div>
-            {/* Sección 5 */}
-            <div className="mb-5">
-              <h3 className="text-2xl font-bold mb-3">5. Consejos para Elegir el Letrero Perfecto</h3>
-              <ul className="list-disc pl-6 mb-4">
-                <li>Opta por colores que reflejen la personalidad de tu bar.</li>
-                <li>Elige un diseño legible y atractivo.</li>
-                <li>Considera el lugar de instalación para maximizar su impacto.</li>
-              </ul>
-              
-            </div>
 
+          <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 mt-16'>
+            {[{
+              title: "El Factor Sorpresa y Distinción",
+              text: "Las letras de neón LED permiten personalizar la imagen de tu local, haciendo que el nombre de tu bar sea visible desde lejos. Un diseño llamativo puede convertirse en un sello distintivo y en un punto de referencia para los clientes."
+            }, {
+              title: "Ambiente y Experiencia Visual",
+              text: "La iluminación juega un papel crucial en la atmósfera de un bar. Los colores vibrantes y cálidos del neón LED pueden transformar un espacio ordinario en un entorno acogedor e instagrameable."
+            }, {
+              title: "Eficiencia Energética y Durabilidad",
+              text: "A diferencia del neón tradicional, las luces LED son más eficientes, consumen menos energía y tienen una vida útil más prolongada."
+            }, {
+              title: "Marketing y Atracción de Clientes",
+              text: "Un letrero de neón LED bien diseñado es una herramienta de marketing poderosa, capaz de captar la atención y aumentar la visibilidad de tu local."
+            }].map((section, index) => (
+              <div key={index} className="p-4 bg-gray-900 rounded-lg shadow-lg">
+                <h3 className="text-2xl font-bold mb-2 text-blue-400">{section.title}</h3>
+                <p className="text-gray-300">{section.text}</p>
+              </div>
+            ))}
           </div>
-          
-          <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 m-5'>
-            <img src="/blog/blog4/image.png" alt="Variedad de letreros neón LED" className="w-full max-w-2xl mb-4 rounded-lg shadow-lg" />
-            <img src="/blog/blog4/hepner1.png" alt="Ambiente con neón en bar" className="w-full max-w-2xl mb-4 rounded-lg shadow-lg" />
+
+          <div className="mt-16 p-6 bg-gray-800 rounded-lg shadow-lg">
+            <h3 className="text-2xl font-bold mb-3 text-green-400">Consejos para Elegir el Letrero Perfecto</h3>
+            <ul className="list-disc pl-6 text-gray-300">
+              <li>Opta por colores que reflejen la personalidad de tu bar.</li>
+              <li>Elige un diseño legible y atractivo.</li>
+              <li>Considera el lugar de instalación para maximizar su impacto.</li>
+            </ul>
           </div>
-          
-          
-          
-          
-          {/* Conclusión */}
-          <div className="mb-4">
-            <h3 className="text-2xl font-bold mb-3">Conclusión</h3>
-            <p className="mb-4">
+
+          <div className='grid grid-cols-1 sm:grid-cols-2 gap-6 mt-10'>
+            {["/blog/blog4/image.png", "/blog/blog4/hepner1.png"].map((src, index) => (
+              <img key={index} src={src} alt="Ejemplo de letrero" className="w-full rounded-lg shadow-xl border-2 border-purple-400" />
+            ))}
+          </div>
+
+          <div className="mt-10 p-6 bg-gray-900 rounded-lg shadow-lg">
+            <h3 className="text-2xl font-bold mb-3 text-yellow-400">Conclusión</h3>
+            <p className="text-gray-300">
               Invertir en luces neón LED no solo mejora la estética de tu bar, sino que también influye en la percepción de los clientes y fortalece tu marca. ¡Haz que tu bar brille con luz propia!
             </p>
-          </div>
-          
-          {/* Imágenes de ejemplos */}
-          <div className="mb-10">
-            <p className="text-sm text-white">
-              A continuación, te presentamos unos ejemplos de nuestros productos:
-            </p>
-            <div className="flex flex-wrap gap-4 justify-center align-middle">
-              <img 
-                src="/blog/blog4/Frame7.png" 
-                alt="Ejemplo 1" 
-                className="w-full max-w-xs rounded-lg shadow-md"
-              />
-              <img 
-                src="/blog/blog4/Frame8.png" 
-                alt="Ejemplo 2" 
-                className="w-full max-w-xs rounded-lg shadow-md"
-              />
-            </div>
           </div>
         </div>
       </div>
